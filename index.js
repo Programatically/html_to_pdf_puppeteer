1713392f31bed6c6f3f2b0cbab56b6332fc097b0
DaniyalAkbar <daniyalakbar1217@gmail.com>
2022-01-31 15:02:49 +0500

    added dynamic_pdf_config & Docker setup

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const service = require('./service')
-app.use(express.json());
+const service = require('./services')
+const cors = require('cors')
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(cors());
 
 
 app.get("/test", service.hello_world);
--- service.js
+++ service.js
@@ -68,3 +68,93 @@ exports.generate_pdf_with_css = async (req, res) => {
   res.contentType("application/pdf");
   res.send(pdf);  
 };
+
+
+
+
+exports.generate_dynamic_pdf = async (req, res) => {
+  try {
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) { res.send("No Request Body Found!"); return }
+
+    const data = req.body;
+
+    let html;
+    var logo_bin = '';
+    var heading = 'Main Heading';
+    var header_content = '';
+    var footer_content = '';
+    var margins = ["200px", "150px", "20px", "20px"];
+
+
+    // Extracting Raw HTML From request-body
+    if (!data.hasOwnProperty("html") || data.html == "") {
+      res.send("No HTML Found!");
+      return;
+    } else {
+      html = data.html;
+    }
+
+    if (data.hasOwnProperty('logoBin') && data.logoBin.length > 0) { logo_bin = data.logoBin; } 
+    if (data.hasOwnProperty('mainHeading') && data.mainHeading.length > 0) { heading = data.mainHeading; } 
+    if (data.hasOwnProperty('headerFooterCss') && data.headerFooterCss.length > 0) { fs.writeFile( "./header_footer.css", data.headerFooterCss, ()=>null ) }
+    if (data.hasOwnProperty('headerContent') && data.headerContent.length > 0) { header_content = util.format(data.headerContent, logo_bin, heading); } 
+    if (data.hasOwnProperty('footerContent') && data.footerContent.length > 0) { footer_content = data.footerContent; }
+    if (data.hasOwnProperty('pdfMargins') && data.pdfMargins.length > 0) { margins = data.pdfMargins.split(';'); }
+
+    const browser = await puppeteer
+      .launch({ headless: true, args: ["--no-sandbox"] })
+      .catch(console.error);
+    const page = await browser.newPage().catch(console.error);
+
+    //  ADDING CSS TO HTML BODY CONTENT
+    html = `<style> ${fs.readFileSync("./html_body.css", "utf8")} </style>
+  ${data.html.replace(/{{'/g, "").replace(/'}}/g, "")}`;
+
+    await page
+      .setContent(html, {
+        waitUntil: "domcontentloaded",
+      }).catch(console.error);
+
+    //  ADDING HEADER AND FOOTER CSS
+    var cssb = [];
+    cssb.push(
+      `<style> ${fs.readFileSync("./header_footer.css", "utf8", (err, data) =>
+        data.toString()
+      )} </style>`
+    );
+    const header_footer_css = cssb.join("");
+
+    const pdf = await page
+      .pdf({
+        format: "A4",
+        printBackground: false,
+        preferCSSPageSize: true,
+        displayHeaderFooter: true,
+
+        headerTemplate: header_footer_css + header_content,
+        footerTemplate: footer_content,
+        margin: {
+          top: margins[0],
+          bottom: margins[1],
+          right: margins[2],
+          left: margins[3],
+        },
+      })
+      .catch(console.error);
+
+    //  SENDING BACK PDF IN RESPONCE TO API CALL
+    res.contentType("application/pdf");
+    await browser.close()
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    // res.header('Access-Control-Allow-Origin: your origin');
+    // res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    // res.header('Access-Control-Allow-Credentials: true');
+    // res.header('Access-Control-Max-Age:86400');
+    // res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token,  Accept, Authorization, X-Requested-With');
+    res.send(pdf);
+  } catch (err) {
+    console.log(err.toString());
+    res.send({"error": err.toString()});
+  }
+};
